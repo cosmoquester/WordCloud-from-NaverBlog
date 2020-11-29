dd34c465dbbe5801da23d603d09f0138d76b8939
박상준 <psj8252@naver.com>
2020-11-29 17:26:31 +0900

    feat: Add parameter to fileter posts by datetime

--- examples/sample2.py
+++ examples/sample2.py
@@ -2,18 +2,19 @@
 You can filter posts in making word cloud.
 """
 from nbclouder import Clouder
+from datetime import datetime, timedelta
 
 # Fill parameters properly
 naver_id = ""
 
 clouder = Clouder(naver_id)
 post_ids = clouder.get_post_ids(["전체글"])
-contents = clouder.get_contents(post_ids, without_datetime=False)
+contents = clouder.get_contents(
+    post_ids, datetime_filter_fn=lambda post_datetime: post_datetime < datetime.now() - timedelta(days=30)
+)
 
 # Filtered by post datetime
-word_frequency = clouder.get_word_frequency(
-    [sent for date_time, sent in contents if date_time.year not in (2018, 2017)], white_tags=["Noun"]
-)
+word_frequency = clouder.get_word_frequency(contents, white_tags=["Noun"])
 
 word_cloud = clouder.make_cloud("word_cloud.png", word_frequency, "/Library/Fonts/Arial Unicode.ttf")
 # Then 'word_cloud.png' is saved
